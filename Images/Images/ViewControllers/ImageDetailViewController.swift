096a52628b781f47c59db5faf0d8c2dce994567c
OleksiiKolomiiets <Oleksii.Kolomiets@onseo.biz>
2018-09-10 15:22:30 +0300

    fixed issue with scroll view and 'done' button

--- Images/Images/ViewControllers/ImageDetailViewController.swift
+++ Images/Images/ViewControllers/ImageDetailViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ImageDetailViewController: UIViewController, UIScrollViewDelegate {
 
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            self.scrollView.addSubview(self.imageView)
+        }
+    }
     @IBOutlet weak var loadActivityIndicator: UIActivityIndicatorView!
     
     var imageData: ImageViewEntity!
+    var imageView = UIImageView()
+    var image: UIImage? {
+        didSet {
+            self.imageView.image = image
+            self.imageView.sizeToFit()
+            self.scrollView.contentSize = self.imageView.frame.size
+        }
+    }
         
     @IBAction func done(_ sender: UIButton) {
         self.dismiss(animated: true)
@@ -23,7 +34,7 @@ class ImageDetailViewController: UIViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.maximumZoomScale = 1.2
-        scrollView.minimumZoomScale = 1/3
+        scrollView.minimumZoomScale = 1/2
         scrollView.delegate = self
         if imageView.image == nil {
             fetchImage()
@@ -40,19 +51,16 @@ class ImageDetailViewController: UIViewController, UIScrollViewDelegate {
     
     private func fetchImage() {
         let sizedPhotoUrl = ImageService.getUrlForPhoto(using: imageData)
-        print(sizedPhotoUrl)
-        print(imageData.url)
         
-        if let image = ImageLoadHelper.getImageFromCache(by: sizedPhotoUrl) {
-            imageView.image = image
+        if let cachedImage = ImageLoadHelper.getImageFromCache(by: sizedPhotoUrl) {
+            self.image = cachedImage
         } else {
             loadActivityIndicator.startAnimating()
-            ImageLoadHelper.getImage(by: sizedPhotoUrl) { image in
+            ImageLoadHelper.getImage(by: sizedPhotoUrl) { loadedImage in
                 self.loadActivityIndicator.stopAnimating()
-                self.imageView.image = image
+                self.image = loadedImage
             }
         }
     }
-    
 
 }
