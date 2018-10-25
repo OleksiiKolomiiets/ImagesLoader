da5a5dac45dffef5960cc6e414e58f8f641df35c
OleksiiKolomiiets <Oleksii.Kolomiiets@onseo.biz>
2018-10-25 17:57:12 +0300

    added view controller and storyboard files for favorite images.

---
+++ Images/Images/ViewControllers/FavoriteImagesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoriteImagesViewController.swift
+//  Images - iOS Application
+//
+//  Created by Oleksii  Kolomiiets on 10/25/18.
+//  Copyright © 2018 Oleksii  Kolomiets. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteImagesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
